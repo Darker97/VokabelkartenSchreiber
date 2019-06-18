674556a9b66b13951d95fcde4b9bc3ab236e4f81
Christian Baltzer <christian@baltzeer.de>
2019-06-18 16:12:22 +0200

    init und finish

---
+++ Code/VokabelSchreiber.java
@@ -0,0 +1,74 @@
+import java.io.FileWriter;
+import java.io.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+class VokabelSchreiber{
+
+    public static Scanner sc = new Scanner(System.in);
+    public static FileWriter fw = null;
+    public static BufferedWriter bw = null;
+
+
+    //Liest die Daten von der Tastatur ein und gibt Sie weiter
+    public static void main(String[] args) {
+        System.out.println("Willkommen!\nBitte trage deine Karten in der Form Name, Frage, Antwort 1 und Antwort 2 ein. \nGibt es keine zweite Antwort, so lassen Sie das Feld frei."); 
+        
+        try {
+            fw = new FileWriter("Data.txt");
+            bw = new BufferedWriter(fw);
+        } catch (Exception e) {
+            System.out.println("ne, wir nichts");
+        }
+
+        while (true){
+            String Temp_String = "";
+            System.out.println("---------------------------------------------------------------");
+            System.out.println("Name?");
+            String Name = sc.nextLine();
+            if(Name.isEmpty()){
+                Temp_String += Name;
+                Temp_String += "/";
+            }
+
+
+            System.out.println("Frage?");
+            Temp_String += sc.nextLine();
+            Temp_String += "/";
+
+            
+            System.out.println("Antwort?");
+            String Antwort = "";
+            Temp_String += sc.nextLine();
+
+            while(true){
+                System.out.println("nächste Antwort");
+                Antwort = sc.nextLine();
+                if (Antwort.isEmpty()){
+                    break;
+                }
+                Temp_String += ";" + Antwort;
+            }
+            schreiben(Temp_String);
+        }
+    }
+
+    // Schreibt den übergebenen String in die Datei
+
+    public static void schreiben(String Text) {
+
+        System.out.println(Text);
+        try {
+            bw.write(Text);
+            bw.write("\n");
+            bw.flush();
+        } catch (Exception e) {
+            System.out.println("AHHHHHHHHH");
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-VokabelkartenSchreiber
+# VokabelkartenSchreiber
+
+Ein kleines Script fürs Terminal mit dem man Vokabeln für die App Lernbox ins richtige Format bringt
+
+### Format
+"Name / Frage / Antwort 1; Antwort 2"
